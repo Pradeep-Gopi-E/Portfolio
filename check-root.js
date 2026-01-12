e7c1e9aa1c064e54a1e33f611ba52c6ceb2d5d2c
Pradeep <pradeepgopi99@gmail.com>
2026-01-12 15:23:21 +0100

    feat: update assets - Cleaned up temporary files and verified project structure"

--- check-root.js
+++
@@ -1,8 +0,0 @@
-try {
-    const icons = require('simple-icons');
-    console.log('Root keys count:', Object.keys(icons).length);
-    const amazon = Object.keys(icons).filter(k => k.match(/amazon/i));
-    console.log('Root Amazon matches:', amazon);
-} catch (e) {
-    console.error(e);
-}
--- dump-keys.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const icons = require('simple-icons/icons');
-fs.writeFileSync('keys.txt', JSON.stringify(Object.keys(icons), null, 2));
-console.log('Done');
